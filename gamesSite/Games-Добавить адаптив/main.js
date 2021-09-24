c4ecc983ae85c8001ce893b1145c4547fcf475fc
Meison12345 <69295331+Meison12345@users.noreply.github.com>
2021-09-24 14:25:57 +0300

    Add files via upload

--- gamesSite/Games-Добавить адаптив/main.js
+++ gamesSite/Games-Добавить адаптив/main.js
@@ -4,9 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
     let nav_ul = document.querySelector(".nav_ul");
     burger.addEventListener("click", function() {
         this.classList.toggle("open");
-        // this.classList.toggle("burger__span-active");
-
-
         nav_ul.classList.toggle("nav_ul-active");
         document.body.classList.toggle("body-active");
     });
@@ -22,48 +19,45 @@ document.addEventListener("DOMContentLoaded", function() {
             inp__email.value.trim().includes("@") &&
             inp__email.value.trim().includes(".")
         ) {
-            console.log('Спасибо, мы свяжемся с вами!')
+            console.log("Спасибо, мы свяжемся с вами!");
         }
     });
 
     let btn_subs = document.querySelector(".subscribe__a");
     let inp_email = document.querySelector(".subscribe__email");
     let servResponse = document.querySelector(".form__active");
-    btn_subs.addEventListener("click", function(e) {
-        e.preventDefault();
-        if (
-            inp_email.value.trim().length > 5 &&
-            inp_email.value.trim().includes("@") &&
-            inp_email.value.trim().includes(".")
-        ) {
-            // let userInput = document.forms.ourForm.ourForm__inp.value;
-            // userInput = encodeURIComponent(userInput);
-            // inp_email.classList.add("inp_anim");
-            // inp_email.insertAdjacentHTML('afterbegin', inp_email.value)
-            // inp_email.disabled = true;
-            document.querySelector(".form__active").classList.add("form-active");
-            document.querySelector(".subscribe__form").classList.add("no-active");
-            // this.disabled = true;
-            //AJAX
-            // let servResponse = document.querySelector('.form__active')
-            let userInput = document.forms.form.email.value;
-            userInput = encodeURIComponent(userInput);
+    if (btn_subs !== null) {
+        btn_subs.addEventListener("click", function(e) {
+            e.preventDefault();
+            if (
+                inp_email.value.trim().length > 5 &&
+                inp_email.value.trim().includes("@") &&
+                inp_email.value.trim().includes(".")
+            ) {
+                document.querySelector(".form__active").classList.add("form-active");
+                document.querySelector(".subscribe__form").classList.add("no-active");
+                let userInput = document.forms.form.email.value;
+                userInput = encodeURIComponent(userInput);
 
-            let xhr = new XMLHttpRequest();
+                let xhr = new XMLHttpRequest();
 
-            xhr.open("POST", "form.php");
+                xhr.open("POST", "form.php");
 
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.setRequestHeader(
+                    "Content-Type",
+                    "application/x-www-form-urlencoded"
+                );
 
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    servResponse.textContent = xhr.responseText;
-                }
-            };
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        servResponse.textContent = xhr.responseText;
+                    }
+                };
 
-            xhr.send("email" + userInput);
-        } else {
-            console.log('Введите корректный email')
-        }
-    });
+                xhr.send("email" + userInput);
+            } else {
+                console.log("Введите корректный email");
+            }
+        });
+    }
 });
