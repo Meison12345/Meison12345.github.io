ab5cc6292eccbd43dde1ca60451495d0c939dd1b
Meison <69295331+Meison12345@users.noreply.github.com>
2022-03-28 12:57:24 +0300

    Add files via upload

--- Test work/js/slider.js
+++ Test work/js/slider.js
@@ -2,11 +2,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const images = document.querySelectorAll('.header__left img');
     const sliderLine = document.querySelector('.slider-line');
-    let count = 0;
+    let sliderRight = document.querySelector('.slider-title-right');
+    let sliderLeft = document.querySelector('.slider-title-left');
+    let count = 0,
+        subcount = 1;
     let width, height;
 
     function init() {
-        console.log('resize');
+        // console.log('resize');
         width = document.querySelector('.header__left').offsetWidth;
         // height = document.querySelector('.header__left').offsetHeight;
         sliderLine.style.width = width * images.length + 'px';
@@ -22,6 +25,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.querySelector('.slider-next').addEventListener('click', function() {
         count++;
+        // console.log(subcount);
+        if (subcount >= images.length - 1) {
+            sliderLeft.textContent = images[subcount - 1].dataset.name;
+            subcount = 0;
+            sliderRight.textContent = images[subcount].dataset.name;
+
+        } else {
+            sliderRight.textContent = images[subcount + 1].dataset.name;
+            if (subcount === 0) {
+                sliderLeft.textContent = images[images.length - 1].dataset.name;
+            } else {
+                sliderLeft.textContent = images[subcount - 1].dataset.name;
+            }
+            subcount++;
+        }
+
         if (count >= images.length) {
             count = 0;
         }
@@ -30,6 +49,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.querySelector('.slider-previous').addEventListener('click', function() {
         count--;
+        subcount--;
+        console.log(subcount);
+
+        if (subcount === 0) {
+            sliderLeft.textContent = images[images.length - 2].dataset.name;
+            sliderRight.textContent = images[subcount].dataset.name;
+        } else if (subcount < 0) {
+            subcount = images.length - 1
+            sliderLeft.textContent = images[subcount - 2].dataset.name;
+            sliderRight.textContent = images[subcount].dataset.name;
+            console.log(subcount);
+        } else if (subcount === 1) {
+            sliderLeft.textContent = images[images.length - 1].dataset.name;
+            sliderRight.textContent = images[subcount].dataset.name;
+
+        } else {
+            sliderLeft.textContent = images[subcount - 2].dataset.name;
+            sliderRight.textContent = images[subcount].dataset.name;
+        }
+
         if (count < 0) {
             count = images.length - 1;
         }
