f00ffad9541f1f1152cdea0ec595b183298f5a90
Meison <69295331+Meison12345@users.noreply.github.com>
2024-09-19 22:34:05 +0300

    Add files via upload

---
+++ test/map.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+	async function initMap() {
+		// Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+		await ymaps3.ready;
+
+		const {
+			YMap,
+			YMapDefaultSchemeLayer,
+			YMapDefaultFeaturesLayer,
+			YMapMarker,
+		} = ymaps3;
+
+		// const { YMapDefaultMarker } = await ymaps3.import('@yandex/ymaps3-default-ui-theme');
+
+		// Иницилиазируем карту
+		const map = new YMap(
+			document.querySelector('#map'),
+			{
+				location: {
+					center: [37.584731, 54.200534],
+					zoom: 16,
+				},
+				showScaleInCopyrights: true
+			},
+			[
+				// Добавляем слой для отображения схематической карты
+				new YMapDefaultSchemeLayer({}),
+				new YMapDefaultFeaturesLayer({}),
+			],
+		);
+
+		const content = document.createElement('div');
+		content.classList.add('circle');
+
+		const marker = new YMapMarker({
+			coordinates: [37.584731, 54.200534],
+			draggable: false,
+			// title: 'North America',
+			// popup: { content: 'Greenland is the Largest Island in North America and the World' },
+		}, content);
+
+
+
+
+
+		map.addChild(marker);
+	}
+
+	initMap();
+});
